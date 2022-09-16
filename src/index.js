692ef2641a966feae07947ed50a2e70960e41a7a
Angel Pereira <42014734+pereira4ngel@users.noreply.github.com>
2022-09-15 21:54:22 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<header>
+  <h1>To do list</h1>  
+</header>
+<main>
+  <section>
+    <button>Add task</button>
+  </section>
+  <aside>
+    <h3>Correct Number</h3>
+    <h3>Score:</h3>
+    <h3>Highscore:</h3>
+  </aside>
+  <section>
+    <div></div>
+    <button>Check</button>
+  </section>
+  </main>
+`;
